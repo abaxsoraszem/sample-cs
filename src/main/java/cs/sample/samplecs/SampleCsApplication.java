697f24ff5228b8bb4e0c73997c03a33029d624ac
abaxsoraszem <csaba@Csabas-MBP-5.lan>
2018-12-14 16:21:33 +0100

    add some

---
+++ src/main/java/cs/sample/samplecs/SampleCsApplication.java
@@ -0,0 +1,14 @@
+package cs.sample.samplecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCsApplication.class, args);
+	}
+
+}
+
